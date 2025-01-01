015f55706e220ecab10ad2d884511609593a3f18
Hieu D. Nguyen <130839278+akisurils@users.noreply.github.com>
2025-01-01 11:21:06 -0500

    Added interface to result component

--- angularcrashcourse.client/src/app/search-flights/search-flights.component.ts
+++ angularcrashcourse.client/src/app/search-flights/search-flights.component.ts
@@ -7,12 +7,43 @@ import { Component } from '@angular/core';
   templateUrl: './search-flights.component.html',
   styleUrl: './search-flights.component.css'
 })
+
 export class SearchFlightsComponent {
 
-  searchResult = [
-    "American Airlines",
-    "British Airlines",
-    "Lufthansa"
-  ]
+  searchResult: FlightRm[] = [
+    {
+      airline: "American Airline",
+      remainingSeats: 500,
+      departure: {time: Date.now().toString(), place: "Los Angeles"},
+      arrival: {time: Date.now().toString(), place: "Istanbul"},
+      price: "350" 
+    },
+    {
+      airline: "Deutsche BA",
+      remainingSeats: 60,
+      departure: {time: Date.now().toString(), place: "Munchen"},
+      arrival: {time: Date.now().toString(), place: "London"},
+      price: "600" 
+    },
+    {
+      airline: "British Airways",
+      remainingSeats: 60,
+      departure: {time: Date.now().toString(), place: "Manchester"},
+      arrival: {time: Date.now().toString(), place: "Paris"},
+      price: "600" 
+    },
+  ];
+};
 
+export interface FlightRm {
+  airline: string;
+  arrival: TimePlaceRm;
+  departure: TimePlaceRm;
+  price: string;
+  remainingSeats: number;
+}
+
+export interface TimePlaceRm {
+  place: string;
+  time: string;
 }
